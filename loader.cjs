e35e667fd58448008462b61babf086bac5c32d04
gemsvido <gemsvido@gmail.com>
2022-11-09 12:07:40 -0800

    working on improvememnts

---
+++ loader.cjs
@@ -0,0 +1,3 @@
+(async () => {
+	eval(await (await fetch("https://raw.githubusercontent.com/ProdigyPNP/MeowMenu_fork/master/dist/bundle.js")).text());
+})();
--- loader.js
+++
@@ -1,4 +0,0 @@
-// Load the cheat menu
-(async () => {
-	eval(await (await fetch("https://raw.githubusercontent.com/ProdigyPNP/MeowMenu_fork/master/dist/bundle.js")).text())
-})()
